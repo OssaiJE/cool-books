dc349cc99fa4861acd9a4901fd831cd9a245dbf5
OssaiJE <ossaije1@gmail.com>
2022-04-30 23:16:03 +0100

    Event based microservice now works

--- BookService/src/book/helpers/create-swap.event.ts
+++ BookService/src/book/helpers/create-swap.event.ts
@@ -3,5 +3,8 @@ export class CreateSwapEvent {
     public readonly userid: string,
     public readonly title: string,
     public readonly author: string,
+    public readonly shortdesc: string,
+    public readonly typetag: string,
+    public readonly lookingfor: string,
   ) {}
 }
