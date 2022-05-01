6d4d521c84948f8752dfd9386a6525154b671e0c
OssaiJE <ossaije1@gmail.com>
2022-05-01 23:29:28 +0100

    Event based microservice now works

--- BookService/src/book/book.service.ts
+++ BookService/src/book/book.service.ts
@@ -3,6 +3,7 @@ import { CreateSwapEvent } from './helpers/create-swap.event';
 
 @Injectable()
 export class BookService {
+  //    @Desc create swap method
   async createSwap(data: CreateSwapEvent) {
     console.log('CreateSwap - BookService', data);
   }
